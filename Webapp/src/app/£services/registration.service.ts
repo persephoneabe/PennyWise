e8626d5d6a2dc1536c7161650771421a5a8d9c87
persephoneabe <abrahamson.persephone@gmail.com>
2020-01-11 15:28:16 -0600

    daily push

--- Webapp/src/app/£services/registration.service.ts
+++ Webapp/src/app/£services/registration.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Component, Injectable, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthenticationService} from './authentication.service';
+import {AlertService} from '../£alerts';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RegistrationService {
+@Component({templateUrl: 'registration.component.html'})
+export class RegistrationService implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private alertService: AlertService
+  ) {
+    // We do not want this page to be accessible to any user that is currently logged in
+    if (this.authenticationService.currentUserValue) {
+      this.router.navigate(['/']);
+    }
+  }
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      fullName: ['', Validators.required],
+       ['', Validators.required],
+
+    })
+  }
 }
